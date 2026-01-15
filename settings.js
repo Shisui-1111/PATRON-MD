dc1f6e3a4be961e1326b47854825d2bab0e8f131
Shisui-1111 <castorpop30@gmail.com>
2026-01-15 21:23:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 //auto presence update
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY" 
 
-global.banknumber = "8133729715"
+global.banknumber = "242066921332"
 
-global.bankowner = "FADARE"
+global.bankowner = "PATRON"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
